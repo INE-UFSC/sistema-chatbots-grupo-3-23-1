7012dd8a0c6cec0f971d985457c8859ab6f7da37
Davi Junkes <davimenegazjunkes@hotmail.com>
2023-04-25 11:29:51 -0300

    Merge remote-tracking branch 'refs/remotes/origin/main'

--- Bots/BotZangado.py
+++ Bots/BotZangado.py
@@ -2,27 +2,26 @@ from Bots.Bot import Bot
 
 class BotZangado(Bot):
     def __init__(self,nome):
-        self.__nome = nome
-
-    #nao esquecer o decorator
-    def nome(self):
-        pass
-
-    #nao esquecer o decorator
-    def nome(nome):
-        pass
+        super.__init__(nome)
+        self._comandos['1'] = "Por que vocẽ é bravo assim ?"
+        self._comandos['2'] = "O que você gosta de fazer?"
+        self._comandos['3'] = "Não fale assim comigo!"
 
     def apresentacao(self):
-        pass
- 
-    def mostra_comandos(self):
-        pass
-    
-    def executa_comando(self,cmd):
-        pass
-
+        return f"Mau dia!, eu sou {self.__nome} quem ousa me incomdodar!?"
+  
+    def executa_comando(self, cmd):
+        if cmd == '1':
+            return "Como assim bravo?! Eu estou completamente calmo!"
+        elif cmd == '2':
+            return "Eu não gosto de falar com você!"
+        elif cmd == '3':
+            return  "Eu falo do jeito que eu quiser!"
+        else:
+            return None
+        
     def boas_vindas(self):
-        pass
+        return "Diga logo o que você quer!"
 
     def despedida(self):
-        pass
+        return "Até que enfim vou embora!"
