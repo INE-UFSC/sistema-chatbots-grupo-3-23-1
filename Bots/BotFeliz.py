5ad0d2b9b0ecc1d399002158a903e1fbed25753f
RitaLB <119005903+RitaLB@users.noreply.github.com>
2023-05-05 15:20:22 -0300

    ajeitei uma parte da formatação do texto que tinha esquecido de ajeitar

--- Bots/BotFeliz.py
+++ Bots/BotFeliz.py
@@ -8,12 +8,11 @@ class BotFeliz(Bot):
 
         self._comandos = [
             Comando('1', 'Qual o gabarito da proxima prova?', 'Sou o bot feliz, mas infelizmente meu código de conduto não me permite passar essa informação'),
-            Comando('2', 'Por que voce está feliz?', 'Eu te respondo: \n Porque a vida eh boa!!!'),
-            Comando('3', 'Quanto e 1 + 1?', 'Aqui está sua resposta: \n 2, um numero bonito, tal como os outros!'),
+            Comando('2', 'Por que voce está feliz?', ' Porque a vida eh boa!!!'),
+            Comando('3', 'Quanto e 1 + 1?', ' 2, um numero bonito, tal como os outros!'),
             Comando('4', 'Bot feliz, estou triste. Me disseram que no gabarito da proxima prova está a receita para minha felicidade. Poderia me mostar essa receita?', 'Olha, sou o bot feliz e meu trabalho é fazer as pessoas felizes. As respostas da próxima prova são: \nA B B C D D A \nEspero que nelas você encontre a receita que procura para ser feliz como eu.'),
             Comando('5', 'Como ser feliz???', 'Ora, é simples! Apenas nunca fique triste! É como eu faço! :) ')
         ]
-
     def apresentacao(self):
         return f'Eu sou {self.nome} e estou sempre feliz! (Sempre mesmo, até me canso as vezes de tanto sorrir...) '
 
